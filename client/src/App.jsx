c9a69d37530b4f24ff704e7643be66ab24bb61c6
2xe2ipii <2xe2ipi@gmail.com>
2025-08-16 16:51:46 +0800

    added react router para sa admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,22 @@
 // Updated client/src/App.jsx
-import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Game from './Game'
 import Admin from './Admin'
 import "./index.css";
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Check if we're on the admin route
-    const path = window.location.pathname;
-    if (path === '/admin') {
-      setIsAdmin(true);
-    }
-  }, []);
-
-  // If not admin, show the game
-  if (!isAdmin) {
-    return (
-      <div className='bg-green-200 w-full h-[100dvh]'>
-        <Game />
-      </div>
-    );
-  }
-
-  // If admin, show admin component
-  return <Admin />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={
+          <div className='bg-green-200 w-full h-[100dvh]'>
+            <Game />
+          </div>
+        } />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
